43f927e6716a8996343bc65925b16e9143a5499a
Rolit Trivedi <rolit454@gmail.com>
2024-03-26 11:47:31 +0530

    Added images on landing page

--- ecommercewebsite/src/Components/Pages/LandingPage.js
+++ ecommercewebsite/src/Components/Pages/LandingPage.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import './LandingPage.css';
+import IntroImg from "../Assets/Images/landingImage.jpeg"
+import SharkTankImg from "../Assets/Images/SharkTank.png"
 import Header from '../Header/Navbar';
 
 class LandingPage extends Component {
@@ -19,7 +21,11 @@ class LandingPage extends Component {
     return (
         <div onClick={() => this.scrollToTop()}>
             <Header/>
-            <video src="https://www.facebook.com/plugins/video.php?height=314&href=https%3A%2F%2Fwww.facebook.com%2Fsharktank.india%2Fvideos%2F2185818911603067%2F&show_text=false&width=560&t=0" width="100%" height="100%" controls="controls" autoplay="true" />
+            <img src={IntroImg} alt='IntroImg' className='LandingIntroImage'/>
+            <hr style={{
+                color: '#F0F1F0',
+                height: '1px',}} />
+            <img src={SharkTankImg} alt='SharkTankImg' className='SharkTankImage' />
         </div>
     )
     }
